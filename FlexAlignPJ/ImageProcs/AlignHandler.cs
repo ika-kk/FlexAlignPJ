b138489255e6d65fc2c33e221dd39d42dd733512
ika <kyouta.k@gmail.com>
2026-01-26 00:32:39 +0900

    [add]マッチング処理追加

---
+++ FlexAlignPJ/ImageProcs/AlignHandler.cs
@@ -0,0 +1,167 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace FlexAlignPJ.ImageProcs
+{
+    internal class AlignHandler : IDisposable
+    {
+        private readonly System.Windows.Media.Color _HistogramColor
+            = System.Windows.Media.Colors.SlateBlue;
+        private readonly int _HistogramWidth = 640;
+        private readonly int _HistogramHeight = 320;
+
+        private Mat _Source;
+        private Mat _Target;
+        private KeyPoint[] _KeyPointsSource;
+        private KeyPoint[] _KeyPointsTarget;
+        private DMatch[] _DMatches;
+        private double _UsePointsRatio;
+
+        /// <summary>各マッチング点の情報のリスト</summary>
+        public ReadOnlySpan<DMatch> DMatches => _DMatches;
+
+        /// <summary>マッチングした点対の数</summary>
+        public int TotalMatchPointsCount => _DMatches.Length;
+
+        /// <summary>アライメントに使うマッチング点の数の割合（0~1で指定）</summary>
+        public double UsePointsRatio
+        {
+            get => _UsePointsRatio;
+            set
+            {
+                if (value < 0) value = 0;
+                if (1 < value) value = 1;
+                _UsePointsRatio = value;
+            }
+        }
+
+        /// <summary>アライメントに使うマッチング点の数。</summary>
+        public int UsePointsCount => (int)(TotalMatchPointsCount * UsePointsRatio);
+
+        /// <summary>距離（マッチングスコア）のヒストグラムを示す画像</summary>
+        public WriteableBitmap MatchScoreHistogramBmp { get; set; }
+
+        /// <summary>マッチング結果の可視化画像</summary>
+        public WriteableBitmap MatchResultBmp { get; set; }
+
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public AlignHandler()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// 各種オブジェクトをDisposeする。
+        /// </summary>
+        public void Dispose()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// プロパティ・フィールドを初期化する。
+        /// </summary>
+        private void Init()
+        {
+            _Source?.Dispose();
+            _Target?.Dispose();
+            _KeyPointsSource = Array.Empty<KeyPoint>();
+            _KeyPointsTarget = Array.Empty<KeyPoint>();
+            _DMatches = Array.Empty<DMatch>();
+            UsePointsRatio = 0.2;
+            MatchScoreHistogramBmp = null;
+            MatchResultBmp = null;
+        }
+
+        /// <summary>
+        /// Source画像とTarget画像を更新する。
+        /// </summary>
+        /// <param name="source">元画像</param>
+        /// <param name="target">合わせ込み先の画像</param>
+        public void MatchImages(WriteableBitmap source, WriteableBitmap target)
+        {
+            _Source = source.ToMat();
+            _Target = target.ToMat();
+            Match(_Source, _Target);
+            UpdateMatchingResultImage();
+        }
+
+        /// <summary>
+        /// マッチング箇所可視化画像を更新する。
+        /// </summary>
+        public void UpdateMatchingResultImage()
+        {
+            using (var matchResult = new Mat())
+            {
+                var debugMatches = _DMatches
+                    .OrderBy(s => s.Distance)
+                    .Take(UsePointsCount)
+                    .ToArray();
+                Cv2.DrawMatches(_Source, _KeyPointsSource, _Target, _KeyPointsTarget, debugMatches, matchResult);
+                MatchResultBmp = matchResult.ToWriteableBitmap();
+            }
+        }
+
+        #region マッチング処理
+
+        /// <summary>
+        /// 2枚の画像の特徴点を検出し、マッチングまで実行する。
+        /// </summary>
+        /// <param name="source">元画像</param>
+        /// <param name="target">合わせ込み先の画像</param>
+        public void Match(Mat source, Mat target)
+        {
+            using (var descriptor1 = new Mat())
+            using (var descriptor2 = new Mat())
+            using (var result = new Mat())
+            using (var akaze = AKAZE.Create())
+            using (var matcher = DescriptorMatcher.Create("BruteForce"))
+            {
+                // 特徴点検出
+                akaze.DetectAndCompute(source, null, out _KeyPointsSource, descriptor1);
+                akaze.DetectAndCompute(target, null, out _KeyPointsTarget, descriptor2);
+                _DMatches = matcher.Match(descriptor1, descriptor2)
+                    .OrderBy(s => s.Distance)
+                    .ToArray();
+                MatchScoreHistogramBmp = GenDistanceHistogramBmp(_DMatches);
+            }
+        }
+
+        /// <summary>
+        /// 距離(マッチングスコア)のヒストグラム関係のプロパティを初期化する。
+        /// </summary>
+        /// <param name="matches"></param>
+        private WriteableBitmap GenDistanceHistogramBmp(DMatch[] matches)
+        {
+            var distances = matches
+                .Select(s => s.Distance)
+                .OrderBy(s => s)
+                .ToArray();
+
+            // 計算用の係数
+            float cx = (distances.Length / (float)_HistogramWidth);
+            float cy = _HistogramHeight / distances.Max();
+
+            // 距離のヒストグラム画像作成
+            var bmp = new WriteableBitmap(_HistogramWidth, _HistogramHeight, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null);
+            for (int x = 0; x < _HistogramWidth; x++)
+            {
+                int i = (int)(cx * x);
+                int y = (int)(cy * distances[i]);
+                bmp.DrawLine(x, _HistogramHeight, x, _HistogramHeight - y - 3, _HistogramColor);
+            }
+            return bmp;
+        }
+
+        #endregion
+    }
+}
--- FlexAlignPJ/ViewModels/MainViewModel.cs
+++ FlexAlignPJ/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using FlexAlignPJ.ImageProcs;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace FlexAlignPJ.ViewModels
 {
     internal partial class MainViewModel : BindableBase
     {
+        private AlignHandler _AlignHandler = new();
 
         #region バインドプロパティ
 
@@ -20,6 +22,12 @@ namespace FlexAlignPJ.ViewModels
         private string _TargetImageFileName = string.Empty;
         private WriteableBitmap _SourceBmp;
         private WriteableBitmap _TargetBmp;
+        private WriteableBitmap _MatchScoreHistogramBmp;
+        private WriteableBitmap _MatchResultBmp;
+        private int _TotalMatchPointsCount = 0;
+        private double _UsePointsRatio = 0.2;
+        private int _UsePointsCount = 0;
+        private double _UsePointsPercent = 20;
 
         /// <summary>Source画像のファイル名</summary>
         public string SourceImageFileName
@@ -45,8 +53,60 @@ namespace FlexAlignPJ.ViewModels
             get => _TargetBmp; set => SetProperty(ref _TargetBmp, value);
         }
 
+        /// <summary>マッチングスコアのヒストグラム画像</summary>
+        public WriteableBitmap MatchScoreHistogramBmp
+        {
+            get => _MatchScoreHistogramBmp; set => SetProperty(ref _MatchScoreHistogramBmp, value);
+        }
+
+        /// <summary>マッチング結果画像</summary>
+        public WriteableBitmap MatchResultBmp
+        {
+            get => _MatchResultBmp; set => SetProperty(ref _MatchResultBmp, value);
+        }
+
+        /// <summary>マッチング点の総数</summary>
+        public int TotalMatchPointsCount
+        {
+            get => _TotalMatchPointsCount; set => SetProperty(ref _TotalMatchPointsCount, value);
+        }
+
+        /// <summary>アライメントに使用するマッチング点の割合(0~1)</summary>
+        public double UsePointsRatio
+        {
+            get => _UsePointsRatio;
+            set
+            {
+                if (value < 0) value = 0;
+                if (1 < value) value = 1;
+                SetProperty(ref _UsePointsRatio, value);
+
+                // 表示用の関連プロパティを更新
+                UsePointsCount = (int)(TotalMatchPointsCount * value);
+                UsePointsPercent = value * 100;
+            }
+        }
+
+        /// <summary>アライメントに使用するマッチング点の数</summary>
+        public int UsePointsCount
+        {
+            get => _UsePointsCount; set => SetProperty(ref _UsePointsCount, value);
+        }
+
+        /// <summary>アライメントに使用するマッチング点の割合[%]</summary>
+        public double UsePointsPercent
+        {
+            get => _UsePointsPercent; set => SetProperty(ref _UsePointsPercent, value);
+        }
+
         #endregion
 
+        #region コマンド
+        /// <summary>
+        /// 画像ファイル参照コマンド
+        /// </summary>
+        /// <param name="sender">source or target</param>
+        /// <exception cref="NotImplementedException"></exception>
         [RelayCommand]
         private void Reference(string sender)
         {
@@ -57,6 +117,7 @@ namespace FlexAlignPJ.ViewModels
                     "bitmap(*.bmp)|*.bmp|" +
                     "png(*.png)|*.png|" +
                     "jpeg(*.jpg;*.jpeg)|*.jpg;*.jpeg";
+                ofd.FilterIndex = 3;
                 if (ofd.ShowDialog() == true)
                 {
                     try
@@ -77,6 +138,7 @@ namespace FlexAlignPJ.ViewModels
                     "bitmap(*.bmp)|*.bmp|" +
                     "png(*.png)|*.png|" +
                     "jpeg(*.jpg;*.jpeg)|*.jpg;*.jpeg";
+                ofd.FilterIndex = 3;
                 if (ofd.ShowDialog() == true)
                 {
                     try
@@ -95,6 +157,51 @@ namespace FlexAlignPJ.ViewModels
                 throw new NotImplementedException();
             }
         }
+
+        /// <summary>
+        /// マッチングを実行する。
+        /// </summary>
+        [RelayCommand]
+        private void ExecuteMatching()
+        {
+            if (SourceBmp is not null &&  TargetBmp is not null)
+            {
+                // マッチング設定・実行
+                _AlignHandler.UsePointsRatio = UsePointsRatio;
+                _AlignHandler.MatchImages(SourceBmp, TargetBmp);
+
+                // 結果取得
+                MatchScoreHistogramBmp = _AlignHandler.MatchScoreHistogramBmp;
+                MatchResultBmp = _AlignHandler.MatchResultBmp;
+                TotalMatchPointsCount = _AlignHandler.TotalMatchPointsCount;
+                UsePointsCount = _AlignHandler.UsePointsCount;
+            }
+            else
+            {
+                MessageBox.Show("Invalid input images.", "Error");
+            }
+        }
+
+        /// <summary>
+        /// 現在の使用点数を反映してマッチング結果画像を更新する。
+        /// </summary>
+        [RelayCommand]
+        private void UpdateMatchingImage()
+        {
+            try
+            {
+                _AlignHandler.UsePointsRatio = UsePointsRatio;
+                _AlignHandler.UpdateMatchingResultImage();
+                MatchResultBmp = _AlignHandler.MatchResultBmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        #endregion
+
 
         /// <summary>
         /// 画像ファイルを読み込む。
